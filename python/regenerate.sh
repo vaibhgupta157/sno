976676a5cfcbccb410292762137780cff3c33b6c
vgcent <vgcent@localhost.localdomain>
2020-10-21 06:39:29 +0530

    latest changes

--- python/regenerate.sh
+++ python/regenerate.sh
@@ -1,3 +1,5 @@
 export PYBINDPLUGIN=`/usr/bin/env python3 -c 'import pyangbind; import os; print ("{}/plugin".format(os.path.dirname(pyangbind.__file__)))'`
 
-pyang --plugindir $PYBINDPLUGIN -f pybind -o sno.py ../yang/* ../device/yang/*
+pyang --plugindir $PYBINDPLUGIN -f pybind -o sno.py ../yang/* ../device/yang/device.yang ../device/yang/CumulusLinux/*
+
+pyang --plugindir $PYBINDPLUGIN -f pybind -o odl.py ../device/yang/odl/*
